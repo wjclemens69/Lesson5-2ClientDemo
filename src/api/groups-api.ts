08ad4ef68835a7b1cff4faca40233d8f4ffdb1a8
wjclemens69 <62310455+wjclemens69@users.noreply.github.com>
2021-01-03 23:35:21 -0500

    adding

---
+++ src/api/groups-api.ts
@@ -0,0 +1,38 @@
+import { GroupModel } from '../types/GroupModel'
+import { apiEndpoint } from '../config'
+import { GroupUploadInfo } from '../types/GroupUploadInfo'
+
+export async function getGroups(): Promise<GroupModel[]> {
+  console.log('Fetching groups')
+
+    //const response = await fetch('${apiEndpoint}/groups')
+
+    const response = await fetch('https://wdvvx1isc4.execute-api.us-east-1.amazonaws.com/Dev/groups')
+
+   
+
+
+    const result = await response.json()
+
+    console.log("result = " + result)
+
+
+  return result.items
+}
+
+export async function createGroup(newGroup: GroupUploadInfo): Promise<GroupModel> {
+
+  const reply = await fetch('${apiEndpoint}/groups', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      name: newGroup.name,
+      description: newGroup.description
+    })
+  })
+  const result = await reply.json();
+  return result.newItem
+}
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const apiEndpoint = 'https://wdvvx1isc4.execute-api.us-east-1.amazonaws.com/Dev'
